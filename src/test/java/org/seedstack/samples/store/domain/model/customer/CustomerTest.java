5b2c3b438836c5cf47f8f3cdf26b8de8d3bfa50e
Rodrigo Menacho <rmenacho21@gmail.com>
2019-03-06 09:54:33 -0400

    Presentation example

---
+++ src/test/java/org/seedstack/samples/store/domain/model/customer/CustomerTest.java
@@ -0,0 +1,33 @@
+package org.seedstack.samples.store.domain.model.customer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomerTest {
+
+    @Test
+    public void getId() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void getFirstName() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void setFirstName() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void getLastName() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void setLastName() {
+        assertTrue(false);
+    }
+}
